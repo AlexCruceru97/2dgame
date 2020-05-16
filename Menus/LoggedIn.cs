070952e7e1ff299355df7418bb96debd628a9d53
cartitableaga <65454972+cartitableaga@users.noreply.github.com>
2020-05-16 17:21:58 +0300

    Add files via upload

---
+++ Menus/LoggedIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoggedIn : MonoBehaviour
+{
+    public TextMeshProUGUI playerDisplay;
+
+    //Shows what user is logged in
+    public void Start()
+    {
+        if (DBManager.LoggedIn)
+        {
+
+            playerDisplay.text = "Player: " + DBManager.username;
+        }
+    }
+    
+    //Logs out the user and goes to main menu
+    public void LogOut()
+    {
+        DBManager.LogOut();
+        SceneManager.LoadScene(0);
+    }
+}
