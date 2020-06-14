142d314cc9ba178e0ef0ac0464ccd120e02f045a
cartitableaga <65454972+cartitableaga@users.noreply.github.com>
2020-06-15 00:33:30 +0300

    Add files via upload

---
+++ CharacterSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public void ChooseCharacter(int characterIndex)
+    {
+        PlayerPrefs.SetInt("SelectedCjaracter", characterIndex);
+    }
+
+
+
+}
